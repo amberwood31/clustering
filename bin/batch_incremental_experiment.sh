bafb03e838c8bb236e39ad1b0349aa692af2a3ed
amber <fang.wu1988@gmail.com>
2019-11-30 19:14:15 -0500

    update script

--- bin/batch_incremental_experiment.sh
+++ bin/batch_incremental_experiment.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 INPUT=$1 # folder path
 FILENAME=$2
 OUTLIER_STRATETY=$3
@@ -16,7 +18,7 @@ do
 	./analyze_clustering.sh $INLIER_QUANTITY $OUTLIER_QUANTITY $OUTLIER_THRESHOLD >&1 | tee /home/amber/stew/slam++/bin/clustering_analysis_"$SEED".txt
 	mv output.txt output_"$SEED".txt
 	mv full_analysis.txt full_analysis_"$SEED".txt
-	mv combined_score_plot.png combined_score_plot_"$SEED".png
+	#mv combined_score_plot.png combined_score_plot_"$SEED".png
     mv result.tga result_"$SEED".tga
     rm sorted.g2o input.g2o
 
@@ -25,5 +27,5 @@ done
 cd ../../test_backend/
 mkdir slampp_"$FILENAME"_"$OUTLIER_STRATETY"
 cd ../slam++/bin/
-cp clustering_output_*.txt clustering_analysis_*.txt output_*.txt full_analysis_*.txt combined_score_plot_*.png result_* ../../test_backend/slampp_"$FILENAME"_"$OUTLIER_STRATETY"
+cp clustering_output_*.txt clustering_analysis_*.txt output_*.txt full_analysis_*.txt result_* ../../test_backend/slampp_"$FILENAME"_"$OUTLIER_STRATETY"
 cp ../../test_backend/analysis_chamber/batch_slampp_plot.sh ../../test_backend/analysis_chamber/plot_g2o_slampp.py ../../test_backend/slampp_"$FILENAME"_"$OUTLIER_STRATETY"
