c328213832ecbb91350a2eb46e53c6017a0dfe80
amber <fang.wu1988@gmail.com>
2019-12-08 15:58:06 -0500

    Updated script

--- bin/batch_slampp_experiment.py
+++ bin/batch_slampp_experiment.py
@@ -1,10 +1,11 @@
 import os
 
 
-dataset = ['manhattan', 'intel', 'mit'] #['csail']
-inliers_quantity = [1952, 256, 20] #[127]
+dataset = ['manhattan']#, 'intel', 'mit'] #['csail']
+inliers_quantity = [1952]#, 256, 20] #[127]
 inliers_percentages = [0.5, 0.6, 0.7, 0.8, 0.9]
 sample_size = 10
+configuration = 'rrandom'
 
 outliers_quantity = []
 for i in range(0, len(dataset)):
@@ -19,7 +20,7 @@ os.chdir('/home/amber/stew/slam++/bin/')
 for i in range(0,len(dataset)):
     for j in range(0, len(inliers_percentages)):
         dataset_name = dataset[i]
-        configuration_name = 'random' + str(outliers_quantity[i][j])
+        configuration_name = configuration + str(outliers_quantity[i][j])
         command = './batch_incremental_experiment.sh /home/amber/stew/pose_dataset/' + dataset_name + '_' + configuration_name + '/ ' + dataset_name + ' ' + configuration_name + ' ' + str(sample_size) + ' ' + str(inliers_quantity[i]) + ' ' + str(outliers_quantity[i][j]) + ' ' + str(0.95)
         print(command)
         os.system(command)
